4e3aefd5cca5106cbf86a0cbee6a927e05d84ff6
TheoH <TheBartenderParadox@gmail.com>
2012-06-06 19:25:42 -0700

    Added timestamps and updated i2c
    Still some timing bugs when running all three sensors.

--- psas-imu/IMU.c
+++ psas-imu/IMU.c
@@ -17,6 +17,7 @@
 #include "lpc23xx-uart.h"
 #include "lpc23xx-util.h"
 #include "lpc23xx-vic.h"
+#include "lpc23xx-timer.h"
 #include "printf-lpc.h"
 
 #include "gfe2368-util.h"
@@ -30,6 +31,10 @@ static axis_data 				accel_data;
 static axis_data				gyro_data;
 static axis_data				mag_data;
 static uint8_t					temp_data;
+
+static uint32_t L3G4200D_timestamp;
+static uint32_t LIS331HH_timestamp;
+static uint32_t LSM303DLH_m_timestamp;
 
 static void poll_wait(i2c_iface i2c_ch) {
 	switch(i2c_ch){
@@ -47,98 +52,109 @@ static void poll_wait(i2c_iface i2c_ch) {
 
 void L3G4200D_get_data_callback(i2c_master_xact_t* caller, i2c_master_xact_t* i2c_s){
 	//axis data has been read, move it to axis data struct
-//	uart0_putstring("\n***CALLBACK***\n");
-	if(!(i2c_s->xact_success)){
-		uart0_putstring("\n***L3G4200D GET DATA FAILED***\n");
-		return;
-	}
-	if(!L3G4200D_data_available(i2c_s->i2c_rd_buffer[1])){
-		uart0_putstring("\n***L3G4200D retrieved old data***\n");
-		//todo: Determine old axis?
-		return;
-	}
+//	if(!(i2c_s->xact_success)){
+//		uart0_putstring("\n***L3G4200D GET DATA FAILED***\n");
+//		return;
+//	}
+//	if(!L3G4200D_data_available(i2c_s->i2c_rd_buffer[1])){
+//		uart0_putstring("\n***L3G4200D retrieved old data***\n");
+//		//todo: Determine old axis?
+//		return;
+//	}
 	temp_data = i2c_s->i2c_rd_buffer[0];
 	gyro_data.x = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[3] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[2];
 	gyro_data.y = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[5] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[4];
 	gyro_data.z = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[7] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[6];
 
-	if(L3G4200D_data_overrun(i2c_s->i2c_rd_buffer[1])){
-		uart0_putstring("\n***L3G4200D data overrun***\n");
-		//todo: Determine old axis?
-	}
-	if(gyro_data.modified == TRUE){
-		uart0_putstring("\n***L3G4200D data not printed since last acquisition***\n");
-	}
+//	if(L3G4200D_data_overrun(i2c_s->i2c_rd_buffer[1])){
+//		uart0_putstring("\n***L3G4200D data overrun***\n");
+//		//todo: Determine old axis?
+//	}
+//	if(gyro_data.modified == TRUE){
+//		uart0_putstring("\n***L3G4200D data not printed since last acquisition***\n");
+//	}
 	gyro_data.modified = TRUE;
 }
 
 void LIS331HH_get_data_callback(i2c_master_xact_t* caller, i2c_master_xact_t* i2c_s){
-	if(!(i2c_s->xact_success)){
-		uart0_putstring("\n***LIS331HH GET DATA FAILED***\n");
-		return;
-	}
-	if(!LIS331HH_data_available(i2c_s->i2c_rd_buffer[0])){
-		uart0_putstring("\n***LIS331HH retrieved old data***\n");
-		return;
-	}
+
+//	if(!(i2c_s->xact_success)){
+//		uart0_putstring("\n***LIS331HH GET DATA FAILED***\n");
+//		return;
+//	}
+//	if(!LIS331HH_data_available(i2c_s->i2c_rd_buffer[0])){
+//		uart0_putstring("\n***LIS331HH retrieved old data***\n");
+//		return;
+//	}
+
+//	uint32_t end = T0TC;
+//	uint32_t tick_per_usec = microsecondsToCPUTicks(1) ;
+//	printf_lpc(UART0, "time to complete sample: %d us\n", (end-timestamp)/tick_per_usec);
+
+
 	accel_data.x = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[2] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[1];
 	accel_data.y = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[4] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[3];
 	accel_data.z = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[6] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[5];
-
-	if(LIS331HH_data_overrun(i2c_s->i2c_rd_buffer[0])){
-		uart0_putstring("\n***LIS331HH data overrun***\n");
-	}
-	if(accel_data.modified == TRUE){
-		uart0_putstring("\n***LIS331HH data not printed since last acquisition***\n");
-	}
+//
+//	if(LIS331HH_data_overrun(i2c_s->i2c_rd_buffer[0])){
+//		uart0_putstring("\n***LIS331HH data overrun***\n");
+//	}
+//	if(accel_data.modified == TRUE){
+//		uart0_putstring("\n***LIS331HH data not printed since last acquisition***\n");
+//	}
 	accel_data.modified = TRUE;
 }
 
 void LSM303DLH_m_get_data_callback(i2c_master_xact_t* caller, i2c_master_xact_t* i2c_s){
-	if(!(i2c_s->xact_success)){
-		uart0_putstring("\n***LSM303DLH_M GET DATA FAILED***\n");
-	}
+//	if(!(i2c_s->xact_success)){
+//		uart0_putstring("\n***LSM303DLH_M GET DATA FAILED***\n");
+//	}
 	mag_data.x = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[0] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[1];
 	mag_data.y = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[2] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[3];
 	mag_data.z = (int16_t)((uint16_t)i2c_s->i2c_rd_buffer[4] << 8) + (uint16_t)i2c_s->i2c_rd_buffer[5];
-	if(mag_data.modified == TRUE){
-		uart0_putstring("\n***LSM303DLH_M data not printed since last acquisition***\n");
-	}
+//	if(mag_data.modified == TRUE){
+//		uart0_putstring("\n***LSM303DLH_M data not printed since last acquisition***\n");
+//	}
 	mag_data.modified = TRUE;
 }
 
 static void empty_callback(i2c_master_xact_t* caller, i2c_master_xact_t* i2c_s) {
 	if(!(i2c_s->xact_success)){
-		uart0_putstring("\n***L3G4200D I2C call failed***\n");
+		uart0_putstring("\n***Some IMU I2C call failed***\n");
 	}
 	return;
 }
 
 void IMU_isr(){
-	DISABLE_GPIO_INT;
-//	uart0_putstring("\n***ISR***\n");
+//	DISABLE_GPIO_INT;
+	uint32_t timestamp = T0TC;
 	//check IOIntStatus bit 0 - LPCUM 10.5.6.1. If 1, PORT0 interrupt.
 	if(!(IOIntStat & 1)){
-		ENABLE_GPIO_INT;
+//		ENABLE_GPIO_INT;
 		return; //interrupt is not for IMU
 	}
 
 	//IO0IntStatR for pin interrupt status
 	//IO0IntClr to clear interrupt
-	if(IO0IntStatR & ACCEL_INT1){
-		LIS331HH_get_data(LIS331HH_get_data_callback);
-		IO0IntClr =	ACCEL_INT1;
-	}
-	if(IO0IntStatR & MAG_DRDY){
-		LSM303DLH_m_get_data(LSM303DLH_m_get_data_callback);
-		IO0IntClr =	 MAG_DRDY;
-	}
-	if(IO0IntStatR & GYRO_INT2){
+	if((IO0IntStatR & GYRO_INT2)){// || L3G4200D_STUCK){
+		L3G4200D_timestamp = timestamp;
 		L3G4200D_get_data(L3G4200D_get_data_callback);
 		IO0IntClr = GYRO_INT2;
 	}
 
-	ENABLE_GPIO_INT;
+	if((IO0IntStatR & ACCEL_INT1)){// || LIS331HH_STUCK){
+		LIS331HH_timestamp = timestamp;
+		LIS331HH_get_data(LIS331HH_get_data_callback);
+		IO0IntClr =	ACCEL_INT1;
+	}
+
+	if((IO0IntStatR & MAG_DRDY)){// || LSM303DLH_M_STUCK){
+		LSM303DLH_m_timestamp = timestamp;
+		LSM303DLH_m_get_data(LSM303DLH_m_get_data_callback);
+		IO0IntClr =	 MAG_DRDY;
+	}
+
+//	ENABLE_GPIO_INT;
 	VICAddress = 0x0;
 }
 
@@ -168,6 +184,10 @@ void IMU_init(){
 	LIS331HH_init(I2C1);
 	LSM303DLH_init_m(I2C2);
 
+    timer_init(TIMER_0, 0x0 , CCLK_DIV1);
+	RESET_TIMER0;
+	START_TIMER0;
+
     VICVectAddr17 = (unsigned int) IMU_isr; //uint?
     ENABLE_GPIO_INT;
 }
@@ -186,15 +206,16 @@ void IMU_task(){
 	accel_data.modified = FALSE;
 
 	//if any of the int lines are high, read data to clear.
-	if(FIO0PIN & MAG_DRDY){
-		LSM303DLH_m_get_data(empty_callback);
-	}
-	if(FIO0PIN & ACCEL_INT1){
-		LIS331HH_get_data(empty_callback);
-	}
-	if(FIO0PIN & GYRO_INT2){
+	if(L3G4200D_STUCK){
 		L3G4200D_get_data(empty_callback);
 	}
+	if(LIS331HH_STUCK){
+		LIS331HH_get_data(empty_callback);
+	}
+	if(LSM303DLH_M_STUCK){
+		LSM303DLH_m_get_data(empty_callback);
+	}
+
 	while(1){
 
 		if(accel_data.modified == TRUE){
@@ -226,21 +247,21 @@ void IMU_task(){
 
 
 int main (void) {
-    pllstart_sixtymhz() ;
+    pllstart_seventytwomhz() ;
     uart0_init_115200() ;
     vic_enableIRQ();
     vic_enableFIQ();
 
     i2c_init(I2C0, DEFAULT);
-    i2c_freq(I2C0, (uint16_t) 100, (uint16_t) 100);//this should be the normal i2c freq...?
+    i2c_kHz(I2C0, 400);
 
     i2c_init(I2C1, I2C1_ALTPIN);
-    i2c_freq(I2C1, (uint16_t) 100, (uint16_t) 100);
+    i2c_kHz(I2C1, 400);
 
     i2c_init(I2C2, DEFAULT);
-    i2c_freq(I2C2, (uint16_t) 100, (uint16_t) 100);
+    i2c_kHz(I2C2, 400);
 
-    uart0_putstring("\n***Starting IMU test (GFE2368)***\n\n");
+    uart0_putstring("\n\n\n***Starting IMU test (GFE2368)***\n\n");
     init_color_led();
     RED_LED_ON;
 
